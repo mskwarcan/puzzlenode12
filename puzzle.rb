38933c326d398e48522ccbf4755d433f5f0bd649
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-08-22 16:38:08 -0500

    Brute forced

--- puzzle.rb
+++ puzzle.rb
@@ -7,97 +7,58 @@ require 'sinatra'
 
 get "/" do
   
-  #constant
-  tax_rate = 1.0411416
+  alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   
-  #convert .json into a hash
-  file = File.open("public/vacation_rentals.json")
-  json = file.readlines.to_s
-  data = ActiveSupport::JSON.decode(json)
-  
-  #grab start and end date
-  file = File.open("public/input.txt")
-  text = file.readlines.to_s
-  dates = text.partition(/ - /)
-  start_date = Time.parse(dates[0]).to_date
-  end_date = Time.parse(dates[2]).to_date
-  
-  resorts = []
-  
-  data.each do |resort|
-    days = (end_date - start_date).to_i
-    rate = 0
-    cleaning_fee = 0
-    
-    name = resort["name"]
-    cleaning_fee = resort["cleaning fee"].nil? ? 0 : resort["cleaning fee"].delete("$").to_f
-    rate = resort["rate"].nil? ? 0 : resort["rate"].delete("$").to_f
-    
-    if resort["seasons"]
-      season_one = resort["seasons"][0]["one"]
-      season_two = resort["seasons"][1]["two"]
-    end
-    
-    if rate != 0
-      #get price if no seasons
-      price = sprintf("%.2f", ((days * rate) + cleaning_fee) * tax_rate)
-    else
-      #season one data
-      season_one_rate = season_one["rate"].delete("$").to_f
-      
-      #figure out if the range is within the same year
-      start_month = season_one["start"].partition(/-/)[0]
-      end_month = season_one["end"].partition(/-/)[0]
-      if start_month.to_i > end_month.to_i
-        end_date_year = "2012-"
-      else 
-        end_date_year = "2011-"
-      end
-      
-      season_one_start = Time.parse("2011-"+season_one["start"]).to_date
-      season_one_end = Time.parse(end_date_year+season_one["end"]).to_date
-      season_one_range = season_one_start..season_one_end
-      
-      #season two data
-      season_two_rate = season_two["rate"].delete("$").to_f
-      
-      #figure out if the range is within the same year
-      start_month = season_two["start"].partition(/-/)[0]
-      end_month = season_two["end"].partition(/-/)[0]
-      if start_month.to_i > end_month.to_i
-        end_date_year = "2012-"
-      else 
-        end_date_year = "2011-"
-      end
-      
-      season_two_start = Time.parse("2011-"+season_two["start"]).to_date
-      season_two_end = Time.parse(end_date_year+season_two["end"]).to_date
-      
-      
-      if season_one_range === start_date
-        if season_one_end < end_date
-          season_one_days = (season_one_end - start_date).to_i + 1
-        else
-          season_one_days = days
-          season_two_price = 0
-        end
-        season_one_price = (season_one_days * season_one_rate)
-        
-        days = days - season_one_days
-      end
-      
-      if days != 0
-        season_two_price = (days * season_two_rate)
-      end
-      
-      price = sprintf("%.2f", (season_two_price + (season_one_price.nil? ? 0 : season_one_price) + cleaning_fee) * tax_rate)
-      
-    end  
-    
-    resorts << [name,price]
+  #Grab text lines
+  lines = []
+  File.open("public/complex_cipher.txt").each do |line|
+      lines << line.strip unless line.strip.empty?
   end
   
-  @total = resorts
+  #Find the amount to shift each letter
+  cypher = lines[0]
+  shift = []
+  cypher.each_char{|char| shift << alphabet.index(char)}
+  
+  #compile the message
+  
+  count = lines.size-1
+  message = ""
+  (1..count).collect {|i|
+    message += lines[i] + " "
+  }
+  
+  different_messages = []
+  
+  (0..26).each {|num|
+    true_message = ""
+    cypher_count = 0
+    
+    message.each_char{|char| 
+      if alphabet.detect  {|i| i == char }
+        if cypher_count == cypher.size
+          cypher_count = 0
+        end
+        new_char = alphabet.index(char).to_i - shift[cypher_count.to_i] + num.to_i
+        if new_char < 0
+          new_char = new_char + 26
+        end
+        
+        if new_char > 25
+          new_char = new_char - 26
+        end
+
+        true_message += alphabet[new_char]
+
+        cypher_count = cypher_count.to_i + 1
+      else
+        true_message += char
+      end
+    }
+    different_messages << true_message
+  }
+  
+  @total = different_messages
   
   erb :total
 end
